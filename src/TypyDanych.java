d71fd781c6ade3db58867df05ca0d4e95926e22c
holi87 <holi87@gmail.com>
2017-02-11 15:14:49 +0100

    typy danych zadanie

---
+++ src/TypyDanych.java
@@ -0,0 +1,15 @@
+
+public class TypyDanych {
+
+	public static void main(String[] args) {
+		String poczatek = "-Cześć Jan, słyszałem, że masz ";
+		String koniec = " lat i uczysz się programowania, czy to prawda?";
+		int wiek = 25;
+		System.out.println(poczatek + wiek + koniec);
+		Boolean prawda = true;
+		System.out.println("-" + prawda);
+		System.out.println("-Czy Twoje inicjały to " + 'J' + " " + 'K' + "?");
+		System.out.print("-Tak, " + prawda);
+	}
+
+}
