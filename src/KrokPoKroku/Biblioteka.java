005e7bc8e152ae6bb41cdb232fa4efc2e78c311d
holi87 <holi87@gmail.com>
2017-02-12 00:28:25 +0100

    biblio v0.3

--- src/KrokPoKroku/Biblioteka.java
+++ src/KrokPoKroku/Biblioteka.java
@@ -3,26 +3,15 @@ package KrokPoKroku;
 public class Biblioteka {
 
 	public static void main(String[] args) {
-		final String nazwaAplikacji = "Biblioteka v0.2";
+		final String nazwaAplikacji = "Biblioteka v0.3";
 		
-		Ksiazka ksiazka1 = new Ksiazka();
-		
-		ksiazka1.tytul = "W pustyni i w puszczy";
-		ksiazka1.autor = "Henryk Sienkieiwcz";
-		ksiazka1.dataWydania = 2010;
-		ksiazka1.iloscStron = 296;
-		ksiazka1.wydawca = "Greg";
-		ksiazka1.isbn = "9788373271890";
+		Ksiazka ksiazka1 = new Ksiazka("W pustyni i w puszczy", "Henryk Sienkieiwcz", 2010, 296, "Greg", "9788373271890");
+		Ksiazka ksiazka2 = new Ksiazka("Pajton", "noName", 2017, 1, "Nope", "lol");
 		
 		System.out.println(nazwaAplikacji);
-		System.out.println("Książki dostępne w bibliotece:");
-		System.out.println(ksiazka1.tytul);
-		System.out.println(ksiazka1.autor);
-		System.out.println(ksiazka1.dataWydania);
-		System.out.println(ksiazka1.iloscStron);
-		System.out.println(ksiazka1.wydawca);
-		System.out.print(ksiazka1.isbn);
-
+		ksiazka1.printInfo();
+		ksiazka2.printInfo();
+		
 	}
 
 }
--- src/KrokPoKroku/Ksiazka.java
+++ src/KrokPoKroku/Ksiazka.java
@@ -8,5 +8,19 @@ public class Ksiazka {
 	String wydawca;
 	String isbn;
 	
+	Ksiazka(String ksiazkaTytul, String ksiazkaAutor, int ksiazkaDataWydania, int ksiazkaIloscStron, String ksiazkaWydawca, String ksiazkaISBN){
+		tytul = ksiazkaTytul;
+		autor = ksiazkaAutor;
+		dataWydania = ksiazkaDataWydania;
+		iloscStron = ksiazkaIloscStron;
+		wydawca = ksiazkaWydawca;
+		isbn = ksiazkaISBN;
+	}
+	
+	void printInfo(){
+		String info = tytul + "; " + autor + "; " + dataWydania + "; " + iloscStron + "; " + wydawca + "; " + isbn;
+		System.out.println(info);
+	}
+	
 
 }
