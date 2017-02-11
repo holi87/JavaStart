4d6d2ba24bfb4902a08ba782d17245087f144027
holi87 <holi87@gmail.com>
2017-02-11 20:11:41 +0100

    x

--- src/Klasy/Company.java
+++ src/Klasy/Company.java
@@ -3,7 +3,7 @@ package Klasy;
 public class Company {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
--- src/Klasy/Employee.java
+++ src/Klasy/Employee.java
@@ -1,5 +1,8 @@
 package Klasy;
 
 public class Employee {
-
+	String imie;
+	String staz;
+	int rokUrodzenia;
+	double stazPracy;
 }
