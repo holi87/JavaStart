dfd3ddfc77545cc33d605ebf7923a0d80edd99e0
holi87 <holi87@gmail.com>
2017-02-11 15:42:32 +0100

    ciąg dalszy

---
+++ src/ZmienneZadanieSamochod.java
@@ -0,0 +1,18 @@
+
+public class ZmienneZadanieSamochod {
+
+	public static void main(String[] args) {
+		String marka;
+		String model;
+		int rocznik;
+		double cena;
+		
+		marka = "Nissan";
+		model = "Micra";
+		rocznik = 1998;
+		cena = 2500.1;
+		
+		System.out.print(marka+" "+model+" z roku "+rocznik+" kosztuje: "+cena+" złotych");
+	}
+
+}
