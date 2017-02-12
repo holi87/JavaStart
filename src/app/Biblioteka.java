0a09655f4f6fb303ada16441e215af3e7cc776d0
holi87 <holi87@gmail.com>
2017-02-12 22:52:34 +0100

    biblio v0.5

--- src/app/Biblioteka.java
+++ src/app/Biblioteka.java
@@ -5,16 +5,16 @@ import data.Ksiazka;
 public class Biblioteka {
 
 	public static void main(String[] args) {
-		final String nazwaAplikacji = "Biblioteka v0.4";
-
-		Ksiazka ksiazka1 = new Ksiazka("W pustyni i w puszczy", "Henryk Sienkieiwcz", 2010, 296, "Greg",
+		final String nazwaAplikacji = "Biblioteka v0.5";
+		Ksiazka[] ksiazki = new Ksiazka[1000];
+		ksiazki[0] = new Ksiazka("W pustyni i w puszczy", "Henryk Sienkieiwcz", 2010, 296, "Greg",
 				"9788373271890");
-		Ksiazka ksiazka2 = new Ksiazka("Pajton", "noName", 2017, 1, "Nope", "lol");
+		ksiazki[1] = new Ksiazka("Pajton", "noName", 2017, 1, "Nope", "lol");
 
 		System.out.println(nazwaAplikacji);
-		ksiazka1.printInfo();
-		ksiazka2.printInfo();
-
+		ksiazki[0].printInfo();
+		ksiazki[1].printInfo();
+		System.out.println("System może przechowywać do " + ksiazki.length + " książek.");
 	}
 
 }
--- src/data/Ksiazka.java
+++ src/data/Ksiazka.java
@@ -8,61 +8,61 @@ public class Ksiazka {
 	private int iloscStron;
 	private String wydawca;
 	private String isbn;
-	
+
 	// settery
-	
-	public void setTytul(String tytul){
+
+	public void setTytul(String tytul) {
 		this.tytul = tytul;
 	}
-	
-	public void setAutor(String autor){
+
+	public void setAutor(String autor) {
 		this.autor = autor;
 	}
-	
-	public void setDataWydania(int dataWydania){
+
+	public void setDataWydania(int dataWydania) {
 		this.dataWydania = dataWydania;
 	}
-	
-	public void setIloscStron(int iloscStron){
+
+	public void setIloscStron(int iloscStron) {
 		this.iloscStron = iloscStron;
 	}
-	
-	public void setWydawca(String wydawca){
+
+	public void setWydawca(String wydawca) {
 		this.wydawca = wydawca;
 	}
-	
-	public void setISBN(String isbn){
+
+	public void setISBN(String isbn) {
 		this.isbn = isbn;
 	}
-	
+
 	// gettery
-	public String getTytul(){
+	public String getTytul() {
 		return tytul;
 	}
-	
-	public String getAutor(){
+
+	public String getAutor() {
 		return autor;
 	}
-	
-	public int getDataWydania(){
+
+	public int getDataWydania() {
 		return dataWydania;
 	}
-	
-	public int getIloscStron(){
+
+	public int getIloscStron() {
 		return iloscStron;
 	}
-	
-	public String getWydawca(){
+
+	public String getWydawca() {
 		return wydawca;
 	}
-	
-	public String getISBN(){
+
+	public String getISBN() {
 		return isbn;
 	}
-	
-	
+
 	// konstruktory
-	public Ksiazka(String ksiazkaTytul, String ksiazkaAutor, int ksiazkaDataWydania, int ksiazkaIloscStron, String ksiazkaWydawca, String ksiazkaISBN){
+	public Ksiazka(String ksiazkaTytul, String ksiazkaAutor, int ksiazkaDataWydania, int ksiazkaIloscStron,
+			String ksiazkaWydawca, String ksiazkaISBN) {
 		this.tytul = ksiazkaTytul;
 		this.autor = ksiazkaAutor;
 		this.dataWydania = ksiazkaDataWydania;
@@ -70,17 +70,17 @@ public class Ksiazka {
 		this.wydawca = ksiazkaWydawca;
 		this.isbn = ksiazkaISBN;
 	}
-	
-	public Ksiazka(Ksiazka ksiazka){
-		this(ksiazka.getTytul(), ksiazka.getAutor(), ksiazka.getDataWydania(), ksiazka.getIloscStron(), ksiazka.getWydawca(), ksiazka.getISBN());
+
+	public Ksiazka(Ksiazka ksiazka) {
+		this(ksiazka.getTytul(), ksiazka.getAutor(), ksiazka.getDataWydania(), ksiazka.getIloscStron(),
+				ksiazka.getWydawca(), ksiazka.getISBN());
 	}
-	
-	
+
 	// wyswietlenie
-	public void printInfo(){
-		String info = getTytul() + "; " + getAutor() + "; " + getDataWydania() + "; " + getIloscStron() + "; " + getWydawca() + "; " + getISBN();
+	public void printInfo() {
+		String info = getTytul() + "; " + getAutor() + "; " + getDataWydania() + "; " + getIloscStron() + "; "
+				+ getWydawca() + "; " + getISBN();
 		System.out.println(info);
 	}
-	
 
 }
